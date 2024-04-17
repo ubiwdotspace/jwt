0df258af9cb103fe1ee96479b63e55e25156cd55
jotaro <nqtien0507@gmail.com>
2024-04-17 20:23:54 +0700

    Enable Cors setting for front-end call

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi import FastAPI,Depends
 from fastapi_simple_rate_limiter import rate_limiter
-
 import uvicorn
 from app.routes.jwt import router as jwt_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
 app.include_router(jwt_router, prefix='/api/v1', tags=['JWT'])
 if __name__ == "__main__":
 
